f4669ba8e59f939a3a03cfb3a5752f886d826cb7
Jennifer Lucifero <jennifer.gommans@codecentric.de>
2020-12-31 12:46:28 +0100

    Optimize for mobile devices

--- src/components/Cell/CellData.tsx
+++ src/components/Cell/CellData.tsx
@@ -2,18 +2,16 @@ import styles from './CellView.module.scss';
 import heartImage from '../../images/heart.png';
 import crossImage from '../../images/cross.png';
 
-const cellSize = 200;
-
 export const heartCell = (
-    <div className={styles['image-border']} style={{height: cellSize + 10, width: cellSize + 10}}>
-        <img style={{height: cellSize, width: cellSize}} src={heartImage} alt="heartImage" />
+    <div className={styles['image-border']}>
+        <img className={styles['image']} src={heartImage} alt="heartImage" />
     </div>
 );
 
 export const crossCell = (
-    <div className={styles['image-border']} style={{height: cellSize + 10, width: cellSize + 10}}>
-        <img style={{height: cellSize, width: cellSize}} src={crossImage} alt="crossImage" />
+    <div className={styles['image-border']}>
+        <img className={styles['image']} src={crossImage} alt="crossImage" />
     </div>
 );
 
-export const notFilledCell = <div className={styles['image-border']} style={{height: cellSize + 10, width: cellSize + 10}} />;
+export const notFilledCell = <div className={styles['image-border']} />;
