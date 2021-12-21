27f916a2c1976dc43e83c00dd15bfee86010925f
Jennifer Lucifero <jennifer.lucifero@codecentric.de>
2021-12-21 11:41:41 +0100

    Create ECS cluster with simple fargate service

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,11 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const bucket = new aws.s3.Bucket("tictactoe-react-frontend", {
-    acl: "public-read",
-    website: {indexDocument: "index.html"}
+// Create a load balancer to listen for requests and route them to the container.
+const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port: 80 });
+
+// Define the service, building and publishing our "./app/Dockerfile", and using the load balancer.
+const service = new awsx.ecs.FargateService("nginx", {
+    desiredCount: 2,
+    taskDefinitionArgs: {
+        containers: {
+            nginx: {
+                image: awsx.ecs.Image.fromPath("nginx", "./app"),
+                memory: 512,
+                portMappings: [listener],
+            },
+        },
+    },
 });
 
-export const bucketName = bucket.id;
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+// Export the URL so we can easily access it.
+export const frontendURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
