ad16bc052f2b309635a38aeede5eba586d52c0f6
Jennifer Lucifero <jennifer.lucifero@codecentric.de>
2021-12-23 08:33:56 +0100

    Rename fargate service

--- deployment/index.ts
+++ deployment/index.ts
@@ -2,15 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import * as awsx from "@pulumi/awsx";
 
 // Create a load balancer to listen for requests and route them to the container.
-const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port: 80 });
+const listener = new awsx.elasticloadbalancingv2.NetworkListener("tictactoe", { port: 80 });
 
 // Define the service, building and publishing our "./app/Dockerfile", and using the load balancer.
-const service = new awsx.ecs.FargateService("nginx", {
+const service = new awsx.ecs.FargateService("tictactoe-frontend", {
     desiredCount: 2,
     taskDefinitionArgs: {
         containers: {
             nginx: {
-                image: awsx.ecs.Image.fromPath("nginx", "../tictactoe-frontend"),
+                image: awsx.ecs.Image.fromPath("tictactoe-frontend", "../tictactoe-frontend"),
                 memory: 512,
                 portMappings: [listener],
             },
